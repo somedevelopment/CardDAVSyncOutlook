3e708c95221e0b6174f49d88125073fd1f7b18a3
Alexander Bikadorov <abiku@cs.tu-berlin.de>
2014-06-13 22:03:10 +0200

    load 32 or 64 bit jacob library depending on platform, internal jacob problem now

--- src/outlook/ManageContactsOutlook.java
+++ src/outlook/ManageContactsOutlook.java
@@ -19,8 +19,16 @@
  */
 package outlook;
 
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.ComThread;
+import com.jacob.com.Dispatch;
+import com.jacob.com.Variant;
+import contact.Contact;
+import contact.Contacts;
+import contact.Contacts.Addressbook;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -31,23 +39,21 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
-
 import main.Status;
-
-import com.jacob.activeX.ActiveXComponent;
-import com.jacob.com.ComThread;
-import com.jacob.com.Dispatch;
-import com.jacob.com.Variant;
-
-import contact.Contact;
-import contact.Contacts;
-import contact.Contacts.Addressbook;
 
 public class ManageContactsOutlook {
 
     static {
+        String workingDir = new File(System.getProperty("user.dir")).getAbsolutePath();
+        String libDir = workingDir + File.separator + "lib";
+        String bitness = System.getProperty("sun.arch.data.model");
+        String libName;
+        if (bitness == "64")
+            libName = "jacob-1.17-x64.dll";
+        else
+            libName = "jacob-1.17-x86.dll";
         try {
-            System.loadLibrary("jacob-1.17-x86");
+            System.load(libDir + File.separator + libName);
         } catch (UnsatisfiedLinkError e) {
             System.err.println("Native code library failed to load.\n" + e);
             System.exit(1);
