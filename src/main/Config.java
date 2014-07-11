72ee5d7dd149b11db496c379e64de9f1db0bf6c6
Alexander Bikadorov <abiku@cs.tu-berlin.de>
2014-07-11 22:47:53 +0200

    added new configuration class, not used yet

---
+++ src/main/Config.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014 Swen Walkowski.
+ * All rights reserved. Originator: Swen Walkowski.
+ * Get more information about CardDAVSyncOutlook at https://github.com/somedevelopment/CardDAVSyncOutlook/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package main;
+
+import java.io.File;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+/**
+ *
+ * @author Alexander Bikadorov <abiku@cs.tu-berlin.de>
+ */
+public final class Config extends PropertiesConfiguration {
+
+    private static Config INSTANCE = null;
+
+    public final static String ACC_USER = "acc.user";
+    public final static String ACC_PASS = "acc.pass";
+    public final static String ACC_SAVE_PASS = "acc.save_pass";
+    public final static String ACC_URL = "acc.url";
+    public final static String ACC_SSL = "acc.ssl";
+    public final static String GLOB_CLOSE = "glob.close";
+
+    private Config() {
+        super();
+    }
+
+    public void saveToFile() {
+        try {
+            this.save();
+        } catch (ConfigurationException e) {
+            System.out.println("can't save configuration");
+            e.printStackTrace();
+        }
+    }
+
+    private static void initialize() {
+
+        INSTANCE = new Config();
+        INSTANCE.setListDelimiter((char) 9);
+
+        //String confDir = "conf";
+        //new File(confDir).mkdir();
+        String filePath = "conf" + File.separator + "config.properties";
+        INSTANCE.setFileName(filePath);
+
+        try {
+            INSTANCE.load(filePath);
+        } catch (ConfigurationException e) {
+            System.out.println("Configuration not found. Using default values");
+        }
+    }
+
+    public static Config getInstance() {
+        if (INSTANCE == null) {
+            initialize();
+        }
+        return INSTANCE;
+    }
+}
+
